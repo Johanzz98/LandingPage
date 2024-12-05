2003e8cfef2d0e304f4271f3590f6d0c40296dc5
Johan <johanzzeroc@gmail.com>
2024-12-04 23:16:03 -0300

    Inicio

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Navbar from "../Components/Navbar/navbar";
+import { Box } from "@mui/material";
+
+
+export default function Page() {
+  return (
+
+          <Box>
+            <Navbar />
+          </Box>
+
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Navba aca</Typography>
+    </Box>
+  );
+};
+
+export default Navbar;
